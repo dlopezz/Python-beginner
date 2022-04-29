e9c313876e597d17b863d38a21bc43481e431479
dlopez <diegol93.dl@gmail.com>
2022-04-29 11:27:03 +0200

    python practice

---
+++ second program.py
@@ -0,0 +1,29 @@
+#print("Hello World \nHello World")
+#print("\n")
+#print("Hello"+"Diego")
+
+
+#Fix the code below 👇
+
+from ctypes.wintypes import CHAR
+
+
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+print("e.g.") 
+print("Hello " + "world")
+print("New lines can be created with a backslash and n.")
+
+#Exercises 3
+#input()
+
+#print("Hello"+ input("Whats your name?"))
+
+#Count
+#CHAR name = [ diego]
+#print("Whats your name\n")
+#input("name")
+
+
+print(len(input("What is your name?\n")))
+
